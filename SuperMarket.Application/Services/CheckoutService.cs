45b1193e55e439722f9b5c018785ccfe3b7e0908
IMRAN SHEIKH <isheikh_2000@hotmail.com>
2025-02-13 09:45:07 +0000

    create CheckoutService to implement ICheckout and handle business logic related to checkout

---
+++ SuperMarket.Application/Services/CheckoutService.cs
@@ -0,0 +1,17 @@
+﻿using SuperMarket.Domain.Interfaces;
+
+namespace SuperMarket.Application.Services
+{
+    public class CheckoutService : ICheckout
+    {
+        public void Scan(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
