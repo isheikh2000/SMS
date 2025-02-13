6159d1206a6297e23faf174e0b366f0ffb2e8b3e
IMRAN SHEIKH <isheikh_2000@hotmail.com>
2025-02-13 09:42:43 +0000

    write a failing unit test Scan_SingleItem_ReturnRegularPrice ()

---
+++ SuperMarket.UnitTests/CheckoutServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using SuperMarket.Application.Services;
+
+namespace SuperMarket.UnitTests;
+
+public class CheckoutServiceTests
+{
+    [SetUp]
+    public void Setup()
+    {        
+    }
+
+    [Test]
+    public void Scan_SingleItem_ReturnRegularPrice()
+    {
+        //Arrange
+        var checkoutService = new CheckoutService();
+
+        //Act
+        checkoutService.Scan("A");        
+        var result = checkoutService.GetTotalPrice();
+
+        //Assert
+        Assert.AreEqual(50, result);
+    }
+}
