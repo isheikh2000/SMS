347d1bf9c5438990609a1e112e011fce3640ba3b
IMRAN SHEIKH <isheikh_2000@hotmail.com>
2025-02-13 10:02:02 +0000

    write minimum domain code to pass the test

--- SuperMarket.Application/Services/CheckoutService.cs
+++ SuperMarket.Application/Services/CheckoutService.cs
@@ -4,14 +4,25 @@ namespace SuperMarket.Application.Services
 {
     public class CheckoutService : ICheckout
     {
+        public Dictionary<string,int> _scanItems;
+
+        public CheckoutService()
+        {
+            _scanItems = new Dictionary<string,int>();
+        }
         public void Scan(string item)
         {
-            throw new NotImplementedException();
+            _scanItems[item] = 1;            
         }
 
         public int GetTotalPrice()
         {
-            throw new NotImplementedException();
+            if (_scanItems.Count > 0 && _scanItems.ContainsKey("A"))
+            {
+                return 50;            
+            }
+            
+            return 0;
         }        
     }
 }
