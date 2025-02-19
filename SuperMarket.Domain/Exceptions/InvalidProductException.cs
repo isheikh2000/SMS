a973d3c4b0c26909614dc155c21a3c9000ba1e3c
IMRAN SHEIKH <isheikh_2000@hotmail.com>
2025-02-19 18:41:10 +0000

    add Invalid Product Exception class

---
+++ SuperMarket.Domain/Exceptions/InvalidProductException.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperMarket.Domain.Exceptions
+{
+    public class InvalidProductException : Exception
+    {
+        public InvalidProductException(string item)
+            : base ($"Invalid Product: {item}")
+        {
+            
+        }
+    }
+}
