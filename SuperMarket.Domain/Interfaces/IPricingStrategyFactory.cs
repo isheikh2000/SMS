1b860abf1ea81cd1d9da2fc56be85ad908f72ce4
IMRAN SHEIKH <isheikh_2000@hotmail.com>
2025-02-19 18:38:17 +0000

    add factory pattern for pricing strategy

---
+++ SuperMarket.Domain/Interfaces/IPricingStrategyFactory.cs
@@ -0,0 +1,14 @@
+﻿using SuperMarket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Domain.Interfaces
+{
+    public interface IPricingStrategyFactory
+    {
+        IPricingStrategy GetPricingStrategy(Product productPrice);
+    }
+}
---
+++ SuperMarket.Domain/Rules/Pricing/PricingStrategyFactory.cs
@@ -0,0 +1,16 @@
+﻿using SuperMarket.Domain.Entities;
+using SuperMarket.Domain.Interfaces;
+
+namespace SuperMarket.Domain.Rules
+{
+    public class PricingStrategyFactory : IPricingStrategyFactory
+    {
+        public IPricingStrategy GetPricingStrategy(Product productPrice)
+        {
+            if (productPrice.SpecialPriceQuantity > 0 && productPrice.SpecialPrice > 0)
+                return new SpecialPricingStrategy(productPrice);
+
+            return new RegularPricingStrategy(productPrice);
+        }
+    }
+}
